653a2c5680a0dd5272074026ffac3bb8df2719ae
Rogerio J. Gentil <rogerio_gentil@yahoo.com.br>
2024-05-22 02:04:45 -0300

    Adds @Repository to the JpaToggleRepository

--- app-srv-sample/src/main/java/rogeriogentil/srv/sample/infrastructure/repository/persistence/JpaToggleRepository.java
+++ app-srv-sample/src/main/java/rogeriogentil/srv/sample/infrastructure/repository/persistence/JpaToggleRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import rogeriogentil.srv.sample.infrastructure.repository.entity.ToggleEntity;
 
 import java.util.Optional;
 
+@Repository
 public interface JpaToggleRepository extends JpaRepository<ToggleEntity, Long> {
 
     Optional<ToggleEntity> findByKeyName(String key);
